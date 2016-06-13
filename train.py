e9f297e7d20e85ac72324019877f4538c4d1d46f
Russell Stewart <Russell.S.Stewart@gmail.com>
2016-06-13 12:40:07 -0700

    Cleaned up code in train.py.

--- train.py
+++ train.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-# 0.1 initialization on all LSTM
-# average pooling on Z
-# decrease regression multiplier
-# 2 layer lstm
-# TODO:
-# reregression
-# dropout on reclassification
-# 3x3 or 2x3 reclassification
 import json
 import datetime
 import random
@@ -17,17 +9,19 @@ import os
 from scipy import misc
 import tensorflow as tf
 import numpy as np
-from tensorflow.models.rnn import rnn_cell
+try:
+    from tensorflow.models.rnn import rnn_cell
+except ImportError:
+    rnn_cell = tf.nn.rnn_cell
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
 
 random.seed(0)
 np.random.seed(0)
 
-from utils import train_utils
-from utils import googlenet_load
+from utils import train_utils, googlenet_load
 
-def build_lstm_inner(lstm_input, H):
+def build_lstm_inner(H, lstm_input):
     '''
     build lstm decoder
     '''
@@ -48,7 +42,7 @@ def build_lstm_inner(lstm_input, H):
             outputs.append(output)
     return outputs
 
-def build_overfeat_inner(lstm_input, H):
+def build_overfeat_inner(H, lstm_input):
     '''
     build simple overfeat decoder
     '''
@@ -59,96 +53,6 @@ def build_overfeat_inner(lstm_input, H):
         w = tf.get_variable('ip', shape=[1024, H['arch']['lstm_size']])
         outputs.append(tf.matmul(lstm_input, w))
     return outputs
-
-def to_idx(vec, w_shape):
-    '''
-    vec = (idn, idh, idw)
-    w_shape = [n, h, w, c]
-    '''
-    return vec[:, 2] + w_shape[2] * (vec[:, 1] + w_shape[1] * vec[:, 0])
-
-def interp(w, i, channel_dim):
-    '''
-    Input:
-        w: A 4D block tensor of shape (n, h, w, c)
-        i: A list of 3-tuples [(x_1, y_1, z_1), (x_2, y_2, z_2), ...],
-            each having type (int, float, float)
- 
-        The 4D block represents a batch of 3D image feature volumes with c channels.
-        The input i is a list of points  to index into w via interpolation. Direct
-        indexing is not possible due to y_1 and z_1 being float values.
-    Output:
-        A list of the values: [
-            w[x_1, y_1, z_1, :]
-            w[x_2, y_2, z_2, :]
-            ...
-            w[x_k, y_k, z_k, :]
-        ]
-        of the same length == len(i)
-    '''
-    w_as_vector = tf.reshape(w, [-1, channel_dim]) # gather expects w to be 1-d
-    upper_l = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.floor(i[:, 1:2]), tf.floor(i[:, 2:3])]))
-    upper_r = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.floor(i[:, 1:2]), tf.ceil(i[:, 2:3])]))
-    lower_l = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.ceil(i[:, 1:2]), tf.floor(i[:, 2:3])]))
-    lower_r = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.ceil(i[:, 1:2]), tf.ceil(i[:, 2:3])]))
-
-    upper_l_idx = to_idx(upper_l, tf.shape(w))
-    upper_r_idx = to_idx(upper_r, tf.shape(w))
-    lower_l_idx = to_idx(lower_l, tf.shape(w))
-    lower_r_idx = to_idx(lower_r, tf.shape(w))
- 
-    upper_l_value = tf.gather(w_as_vector, upper_l_idx)
-    upper_r_value = tf.gather(w_as_vector, upper_r_idx)
-    lower_l_value = tf.gather(w_as_vector, lower_l_idx)
-    lower_r_value = tf.gather(w_as_vector, lower_r_idx)
- 
-    alpha_lr = tf.expand_dims(i[:, 2] - tf.floor(i[:, 2]), 1)
-    alpha_ud = tf.expand_dims(i[:, 1] - tf.floor(i[:, 1]), 1)
- 
-    upper_value = (1 - alpha_lr) * upper_l_value + (alpha_lr) * upper_r_value
-    lower_value = (1 - alpha_lr) * lower_l_value + (alpha_lr) * lower_r_value
-    value = (1 - alpha_ud) * upper_value + (alpha_ud) * lower_value
-    return value
-
-def bilinear_select(H, pred_boxes, early_feat, early_feat_channels, w_offset, h_offset):
-    '''
-    Function used for rezooming high level feature maps. Uses bilinear interpolation
-    to select all channels at index (x, y) for a high level feature map, where x and y are floats.
-    '''
-    grid_size = H['arch']['grid_width'] * H['arch']['grid_height']
-    outer_size = grid_size * H['arch']['batch_size']
-
-    fine_stride = 8. # pixels per 60x80 grid cell in 480x640 image
-    coarse_stride = H['arch']['region_size'] # pixels per 15x20 grid cell in 480x640 image
-    batch_ids = []
-    x_offsets = []
-    y_offsets = []
-    for n in range(H['arch']['batch_size']):
-        for i in range(H['arch']['grid_height']):
-            for j in range(H['arch']['grid_width']):
-                for k in range(H['arch']['rnn_len']):
-                    batch_ids.append([n])
-                    x_offsets.append([coarse_stride / 2. + coarse_stride * j])
-                    y_offsets.append([coarse_stride / 2. + coarse_stride * i])
-
-    batch_ids = tf.constant(batch_ids)
-    x_offsets = tf.constant(x_offsets)
-    y_offsets = tf.constant(y_offsets)
-
-    pred_boxes_r = tf.reshape(pred_boxes, [outer_size * H['arch']['rnn_len'], 4])
-    scale_factor = coarse_stride / fine_stride # scale difference between 15x20 and 60x80 features
-
-    pred_x_center = (pred_boxes_r[:, 0:1] + w_offset * pred_boxes_r[:, 2:3] + x_offsets) / fine_stride
-    pred_x_center_clip = tf.clip_by_value(pred_x_center,
-                                     0,
-                                     scale_factor * H['arch']['grid_width'] - 1)
-    pred_y_center = (pred_boxes_r[:, 1:2] + h_offset * pred_boxes_r[:, 3:4] + y_offsets) / fine_stride
-    pred_y_center_clip = tf.clip_by_value(pred_y_center,
-                                          0,
-                                          scale_factor * H['arch']['grid_height'] - 1)
-
-    interp_indices = tf.concat(1, [tf.to_float(batch_ids), pred_y_center_clip, pred_x_center_clip])
-    return interp_indices
 
 def rezoom(H, pred_boxes, early_feat, early_feat_channels, w_offsets, h_offsets):
     '''
@@ -175,7 +79,7 @@ def rezoom(H, pred_boxes, early_feat, early_feat_channels, w_offsets, h_offsets)
             indices.append(bilinear_select(H, pred_boxes, early_feat, early_feat_channels, w_offset, h_offset))
 
     interp_indices = tf.concat(0, indices)
-    rezoom_features = interp(early_feat, interp_indices, early_feat_channels)
+    rezoom_features = train_utils.interp(early_feat, interp_indices, early_feat_channels)
     rezoom_features_r = tf.reshape(rezoom_features,
                                       [len(w_offsets) * len(h_offsets), outer_size, H['arch']['rnn_len'], early_feat_channels])
     rezoom_features_t = tf.transpose(rezoom_features_r, [1, 2, 0, 3])
@@ -209,9 +113,9 @@ def build_forward(H, x, googlenet, phase, reuse):
         scale_down = 0.01
         lstm_input = tf.reshape(Z * scale_down, (H['arch']['batch_size'] * grid_size, 1024))
         if H['arch']['use_lstm']:
-            lstm_outputs = build_lstm_inner(lstm_input, H)
+            lstm_outputs = build_lstm_inner(H, lstm_input)
         else:
-            lstm_outputs = build_overfeat_inner(lstm_input, H)
+            lstm_outputs = build_overfeat_inner(H, lstm_input)
 
         pred_boxes = []
         pred_logits = []
@@ -281,7 +185,6 @@ def build_forward(H, x, googlenet, phase, reuse):
                 pred_boxes_deltas = tf.concat(1, pred_boxes_deltas)
             return pred_boxes, pred_logits, pred_confidences, pred_confs_deltas, pred_boxes_deltas
 
-    pred_logits = tf.Print(pred_logits, [tf.shape(pred_logits)])
     return pred_boxes, pred_logits, pred_confidences
 
 @ops.RegisterGradient("Hungarian")
@@ -405,7 +308,6 @@ def build(H, q):
          loss[phase], confidences_loss[phase],
          boxes_loss[phase]) = build_forward_backward(H, x, encoder_net, phase, boxes, flags)
         pred_confidences_r = tf.reshape(pred_confidences, [H['arch']['batch_size'], grid_size, H['arch']['rnn_len'], arch['num_classes']])
-        pred_confidences_r = tf.Print(pred_confidences_r, [tf.shape(pred_confidences_r)], summarize=4)
         pred_boxes_r = tf.reshape(pred_boxes, [H['arch']['batch_size'], grid_size, H['arch']['rnn_len'], 4])
 
 
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -178,3 +178,94 @@ def union(box1, box2):
 
 def iou(box1, box2):
     return intersection(box1, box2) / union(box1, box2)
+
+def to_idx(vec, w_shape):
+    '''
+    vec = (idn, idh, idw)
+    w_shape = [n, h, w, c]
+    '''
+    return vec[:, 2] + w_shape[2] * (vec[:, 1] + w_shape[1] * vec[:, 0])
+
+def interp(w, i, channel_dim):
+    '''
+    Input:
+        w: A 4D block tensor of shape (n, h, w, c)
+        i: A list of 3-tuples [(x_1, y_1, z_1), (x_2, y_2, z_2), ...],
+            each having type (int, float, float)
+ 
+        The 4D block represents a batch of 3D image feature volumes with c channels.
+        The input i is a list of points  to index into w via interpolation. Direct
+        indexing is not possible due to y_1 and z_1 being float values.
+    Output:
+        A list of the values: [
+            w[x_1, y_1, z_1, :]
+            w[x_2, y_2, z_2, :]
+            ...
+            w[x_k, y_k, z_k, :]
+        ]
+        of the same length == len(i)
+    '''
+    w_as_vector = tf.reshape(w, [-1, channel_dim]) # gather expects w to be 1-d
+    upper_l = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.floor(i[:, 1:2]), tf.floor(i[:, 2:3])]))
+    upper_r = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.floor(i[:, 1:2]), tf.ceil(i[:, 2:3])]))
+    lower_l = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.ceil(i[:, 1:2]), tf.floor(i[:, 2:3])]))
+    lower_r = tf.to_int32(tf.concat(1, [i[:, 0:1], tf.ceil(i[:, 1:2]), tf.ceil(i[:, 2:3])]))
+
+    upper_l_idx = to_idx(upper_l, tf.shape(w))
+    upper_r_idx = to_idx(upper_r, tf.shape(w))
+    lower_l_idx = to_idx(lower_l, tf.shape(w))
+    lower_r_idx = to_idx(lower_r, tf.shape(w))
+ 
+    upper_l_value = tf.gather(w_as_vector, upper_l_idx)
+    upper_r_value = tf.gather(w_as_vector, upper_r_idx)
+    lower_l_value = tf.gather(w_as_vector, lower_l_idx)
+    lower_r_value = tf.gather(w_as_vector, lower_r_idx)
+ 
+    alpha_lr = tf.expand_dims(i[:, 2] - tf.floor(i[:, 2]), 1)
+    alpha_ud = tf.expand_dims(i[:, 1] - tf.floor(i[:, 1]), 1)
+ 
+    upper_value = (1 - alpha_lr) * upper_l_value + (alpha_lr) * upper_r_value
+    lower_value = (1 - alpha_lr) * lower_l_value + (alpha_lr) * lower_r_value
+    value = (1 - alpha_ud) * upper_value + (alpha_ud) * lower_value
+    return value
+
+def bilinear_select(H, pred_boxes, early_feat, early_feat_channels, w_offset, h_offset):
+    '''
+    Function used for rezooming high level feature maps. Uses bilinear interpolation
+    to select all channels at index (x, y) for a high level feature map, where x and y are floats.
+    '''
+    grid_size = H['arch']['grid_width'] * H['arch']['grid_height']
+    outer_size = grid_size * H['arch']['batch_size']
+
+    fine_stride = 8. # pixels per 60x80 grid cell in 480x640 image
+    coarse_stride = H['arch']['region_size'] # pixels per 15x20 grid cell in 480x640 image
+    batch_ids = []
+    x_offsets = []
+    y_offsets = []
+    for n in range(H['arch']['batch_size']):
+        for i in range(H['arch']['grid_height']):
+            for j in range(H['arch']['grid_width']):
+                for k in range(H['arch']['rnn_len']):
+                    batch_ids.append([n])
+                    x_offsets.append([coarse_stride / 2. + coarse_stride * j])
+                    y_offsets.append([coarse_stride / 2. + coarse_stride * i])
+
+    batch_ids = tf.constant(batch_ids)
+    x_offsets = tf.constant(x_offsets)
+    y_offsets = tf.constant(y_offsets)
+
+    pred_boxes_r = tf.reshape(pred_boxes, [outer_size * H['arch']['rnn_len'], 4])
+    scale_factor = coarse_stride / fine_stride # scale difference between 15x20 and 60x80 features
+
+    pred_x_center = (pred_boxes_r[:, 0:1] + w_offset * pred_boxes_r[:, 2:3] + x_offsets) / fine_stride
+    pred_x_center_clip = tf.clip_by_value(pred_x_center,
+                                     0,
+                                     scale_factor * H['arch']['grid_width'] - 1)
+    pred_y_center = (pred_boxes_r[:, 1:2] + h_offset * pred_boxes_r[:, 3:4] + y_offsets) / fine_stride
+    pred_y_center_clip = tf.clip_by_value(pred_y_center,
+                                          0,
+                                          scale_factor * H['arch']['grid_height'] - 1)
+
+    interp_indices = tf.concat(1, [tf.to_float(batch_ids), pred_y_center_clip, pred_x_center_clip])
+    return interp_indices
+
