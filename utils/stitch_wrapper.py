e12d9924ebb0676af78a28ab197b2efd598386db
Russell Stewart <Russell.S.Stewart@gmail.com>
2016-10-31 20:39:00 -0700

    Fixed typo in stitch_wrapper.py.

--- utils/stitch_wrapper.py
+++ utils/stitch_wrapper.py
@@ -1,1 +1,1 @@
-print('ERROR: stitck_wrapper not yet compiled. Please run `cd /path/to/tensorbox/utils && make`')
+print('ERROR: stitch_wrapper not yet compiled. Please run `cd /path/to/tensorbox/utils && make`')
