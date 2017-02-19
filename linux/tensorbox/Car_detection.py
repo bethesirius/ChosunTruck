98c4949ba22a907763b95a7b26def6a316df2771
Chiwan Song <chi3236@gmail.com>
2017-02-19 17:35:47 +0900

    remove unnecessary parts

--- linux/tensorbox/Car_detection.py
+++ linux/tensorbox/Car_detection.py
@@ -42,19 +42,11 @@ def get_results(args, H):
 
         pred_annolist = al.AnnoList()
 
-        true_annolist = al.parse(args.test_boxes)
         data_dir = os.path.dirname(args.test_boxes)
         image_dir = get_image_dir(args)
-        #subprocess.call('mkdir -p %s' % image_dir, shell=True)
+        subprocess.call('mkdir -p %s' % image_dir, shell=True)
 
-	#ivc = cv2.VideoCapture('/home/caucse/images/ets.mp4')
-	#c=1
-
-	#if vc.isOpened():
-    	#    rval , frame = vc.read()
-	#else:
-	#    rval = False
-
+	
 	memory = sysv_ipc.SharedMemory(123463)
 	memory2 = sysv_ipc.SharedMemory(123464)
 	size = 768, 1024, 3
@@ -63,29 +55,19 @@ def get_results(args, H):
 	pedal.press(1)
 	road_center = 320
 	while True:
-	    #rval, frame = vc.read()
-	    #c = c + 1
-	    #if c % 6 is 0:
-		#    c = c + 1
-	    #time.sleep(0.5)
+	   
 	    cv2.waitKey(1)
 	    frameCount = bytearray(memory.read())
 	    curve = bytearray(memory2.read())
 	    curve = str(struct.unpack('i',curve)[0])
 	    m = np.array(frameCount, dtype=np.uint8)
 	    orig_img = m.reshape(size)
-	    #print orig_img[0]
-	    #cv2.imshow('1', m)
-
-	    #true_anno = true_annolist[i]
-	    #orig_img = imread('%s/%s' % (data_dir, true_anno.imageName))[:,:,:3]
-	    #orig_img = imread('/home/caucse/images/1.jpg')
-	    #orig_img = m
+	   
 	    img = imresize(orig_img, (H["image_height"], H["image_width"]), interp='cubic')
 	    feed = {x_in: img}
 	    (np_pred_boxes, np_pred_confidences) = sess.run([pred_boxes, pred_confidences], feed_dict=feed)
 	    pred_anno = al.Annotation()
-	    #pred_anno.imageName = true_anno.imageName
+	    
 	    new_img, rects = add_rectangles(H, [img], np_pred_confidences, np_pred_boxes,
 					    use_stitching=True, rnn_len=H['rnn_len'], min_conf=args.min_conf, tau=args.tau, show_suppressed=args.show_suppressed)
 	    flag = 0
@@ -108,41 +90,10 @@ def get_results(args, H):
 	    pred_anno.imagePath = os.path.abspath(data_dir)
 	    pred_anno = rescale_boxes((H["image_height"], H["image_width"]), pred_anno, orig_img.shape[0], orig_img.shape[1])
 	    pred_annolist.append(pred_anno)
-	    #imname = '%s/%s' % (image_dir, os.path.basename(true_anno.imageName))
-	    #imname = '/home/caucse/images/_%s.jpg' % (c)
+	    
 	    cv2.imshow('.jpg', new_img)
-	    #misc.imsave(imname, new_img)
-	    #if c % 25 == 0:
-		#print(c)
-
-
-
- 	
-        for i in range(len(true_annolist)):
-            true_anno = true_annolist[i]
-            #orig_img = imread('%s/%s' % (data_dir, true_anno.imageName))[:,:,:3]
-	    orig_img = imread('/home/caucse/images/1.jpg')
-            img = imresize(orig_img, (H["image_height"], H["image_width"]), interp='cubic')
-            feed = {x_in: img}
-            (np_pred_boxes, np_pred_confidences) = sess.run([pred_boxes, pred_confidences], feed_dict=feed)
-            pred_anno = al.Annotation()
-            pred_anno.imageName = true_anno.imageName
-            new_img, rects = add_rectangles(H, [img], np_pred_confidences, np_pred_boxes,
-                                            use_stitching=True, rnn_len=H['rnn_len'], min_conf=args.min_conf, tau=args.tau, show_suppressed=args.show_suppressed)
-            
-	    for rect in rects:
-		print(rect.x1, rect.y1, rect.x2, rect.y2)
-            	
-            pred_anno.rects = rects
-            pred_anno.imagePath = os.path.abspath(data_dir)
-            pred_anno = rescale_boxes((H["image_height"], H["image_width"]), pred_anno, orig_img.shape[0], orig_img.shape[1])
-            pred_annolist.append(pred_anno)
-            #imname = '%s/%s' % (image_dir, os.path.basename(true_anno.imageName))
-	    imname = '/home/caucse/images/_1.jpg'
-            misc.imsave(imname, new_img)
-            if i % 25 == 0:
-                print(i)
-    return pred_annolist, true_annolist
+	    
+    return none;
 
 def main():
     parser = argparse.ArgumentParser()
@@ -162,26 +113,8 @@ def main():
         H = json.load(f)
     expname = args.expname + '_' if args.expname else ''
     pred_boxes = '%s.%s%s' % (args.weights, expname, os.path.basename(args.test_boxes))
-    true_boxes = '%s.gt_%s%s' % (args.weights, expname, os.path.basename(args.test_boxes))
 
-
-    pred_annolist, true_annolist = get_results(args, H)
-    pred_annolist.save(pred_boxes)
-    true_annolist.save(true_boxes)
-
-    try:
-        rpc_cmd = './utils/annolist/doRPC.py --minOverlap %f %s %s' % (args.iou_threshold, true_boxes, pred_boxes)
-        print('$ %s' % rpc_cmd)
-        rpc_output = subprocess.check_output(rpc_cmd, shell=True)
-        print(rpc_output)
-        txt_file = [line for line in rpc_output.split('\n') if line.strip()][-1]
-        output_png = '%s/results.png' % get_image_dir(args)
-        plot_cmd = './utils/annolist/plotSimple.py %s --output %s' % (txt_file, output_png)
-        print('$ %s' % plot_cmd)
-        plot_output = subprocess.check_output(plot_cmd, shell=True)
-        print('output results at: %s' % plot_output)
-    except Exception as e:
-        print(e)
+    get_results(args, H)
 
 if __name__ == '__main__':
     main()
